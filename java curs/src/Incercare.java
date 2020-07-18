8abdd45ee5d51410b32608590ae82711c22499f1
cealex-pixel <alex.ceres@mail.ru>
2020-07-18 11:52:35 +0300

    -tect

---
+++ java curs/src/Incercare.java
@@ -0,0 +1,18 @@
+public class Incercare {
+
+    public static void main(String[] args) {
+
+        String name1 = "Alexandru";
+        String name2 = "Ceres";
+        String name3 = "Nicolae";
+        String name4 = "Sîrbu ";
+
+        String message1 = name1 + " " + name2;
+        String message2 = name3 + " " + name4;
+        String message3 = "My name " + message1 + " and " + "my teacher is " + message2;
+
+        System.out.println(message3);
+    }
+}
+
+
